6fc3701d59868f23b7c5f0361967ade9cac3057f
Julio C. Rojas M <a1jrojas@gmail.com>
2025-12-21 08:47:19 +0000

    Update main page to Home

--- src/pages.config.js
+++ src/pages.config.js
@@ -1,29 +1,29 @@
-import Dashboard from './pages/Dashboard';
-import Payslips from './pages/Payslips';
-import VacationRequest from './pages/VacationRequest';
-import MyProfile from './pages/MyProfile';
-import Certificates from './pages/Certificates';
 import Attendance from './pages/Attendance';
-import ManagerApprovals from './pages/ManagerApprovals';
-import VacationCalendar from './pages/VacationCalendar';
+import Certificates from './pages/Certificates';
+import Dashboard from './pages/Dashboard';
 import Home from './pages/Home';
+import ManagerApprovals from './pages/ManagerApprovals';
+import MyProfile from './pages/MyProfile';
+import Payslips from './pages/Payslips';
+import VacationCalendar from './pages/VacationCalendar';
+import VacationRequest from './pages/VacationRequest';
 import __Layout from './Layout.jsx';
 
 
 export const PAGES = {
-    "Dashboard": Dashboard,
-    "Payslips": Payslips,
-    "VacationRequest": VacationRequest,
-    "MyProfile": MyProfile,
-    "Certificates": Certificates,
     "Attendance": Attendance,
-    "ManagerApprovals": ManagerApprovals,
-    "VacationCalendar": VacationCalendar,
+    "Certificates": Certificates,
+    "Dashboard": Dashboard,
     "Home": Home,
+    "ManagerApprovals": ManagerApprovals,
+    "MyProfile": MyProfile,
+    "Payslips": Payslips,
+    "VacationCalendar": VacationCalendar,
+    "VacationRequest": VacationRequest,
 }
 
 export const pagesConfig = {
-    mainPage: "Dashboard",
+    mainPage: "Home",
     Pages: PAGES,
     Layout: __Layout,
 };
